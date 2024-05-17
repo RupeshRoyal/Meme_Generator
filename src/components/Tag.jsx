6092bce630569a578997b98bcaa7ec921c77817f
Yata Rupesh <99956608+RupeshRoyal@users.noreply.github.com>
2024-05-17 16:21:14 +0530

    cleared warning

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import Reactfrom "react";
 import useGif from "../hooks/useGif";
 import Spinner from "./Spinner";
 // import { useEffect } from "react";
 // import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 
 const Tag = () => {
@@ -36,7 +36,7 @@ const Tag = () => {
     <div className="w-1/2  bg-blue-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="text-3xl uppercase underline font-bold">Random {tag} Gif</h1>
       {
-        loading ? (<Spinner />) : (<img src= {gif} width="450" />)
+        loading ? (<Spinner />) : (<img src= {gif} width="450" alt="../public/apple-touch-icon.png"/>)
       }
 
       <input type="text" className="w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center" 
