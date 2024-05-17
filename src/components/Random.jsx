78f8c1d7c2026290b4318c4f5e7561b50ed911b5
Yata Rupesh <99956608+RupeshRoyal@users.noreply.github.com>
2024-05-17 16:13:18 +0530

    cleared warning

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import useGif from "../hooks/useGif";
 import Spinner from "./Spinner";
 // import { useEffect } from "react";
 // import axios from "axios";
 
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+// const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
   // const [gif, setGif] = useState("");
@@ -33,7 +33,7 @@ const Random = () => {
     <div className="w-1/2  bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
       <h1 className="text-3xl uppercase underline font-bold">A Random Gif</h1>
 
-      {loading ? <Spinner /> : <img src={gif} width="450" />}
+      {loading ? <Spinner /> : <img src={gif} width="450" alt="../public/apple-touch-icon.png"/>}
 
       <button
         onClick={() => fetchData()}
